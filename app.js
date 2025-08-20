cc363b37d18b01d7c48ff78c0b62012d557fa2e9
matheus guedes <guedesmatheus133@gmail.com>
2025-08-19 21:06:26 -0300

    adicionado a logica de adicionar amigo

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-let amigos = [];
+let amigos = []; 
+function adicionarAmigo(){
+   let amigo = document.getElementById("amigo").value;
+   if(amigo.trim() !== ""){
+         addAmigo(amigo);
+         amigo = document.getElementById("amigo").value = "";
+         statusCampo("Amigo adicionado com sucesso!");
+         console.log(amigos);
+   }else{
+        erroDecampo();
+   }
+}
+function addAmigo(amigo){
+    amigos.push(amigo);
+}
+
+function erroDecampo(){
+    statusCampo("Por favor, digite um nome válido.");
+    document.getElementById("amigo").value = "";
+    document.getElementById("amigo").focus();
+}
+
+function statusCampo(mensagem){
+    document.getElementById("status").innerHTML = mensagem;}
+    
 
 

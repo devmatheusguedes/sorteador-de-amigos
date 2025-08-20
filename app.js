f348eddb53951e575880cb0005763094bac56ba2
matheus guedes <guedesmatheus133@gmail.com>
2025-08-19 22:52:58 -0300

    adicionado funcao para tratamento de inputs nao permitidos com cores personalizadas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let amigos = []; // Array para armazenar os amigos
 let amigosSorteados = []; // Array para armazenar os amigos sorteados
 
+
 // Função para adicionar um amigo ao array e atualizar a lista
 function adicionarAmigo(){
    let amigo = document.getElementById("amigo").value;
@@ -29,33 +30,41 @@ function statusCampo(mensagem){
     document.getElementById("status").innerHTML = mensagem;}
 
 function sortearAmigo(){
+    statusCampo("");
     if(amigos.length < 2){
-        mostrarResultado("Adicione pelo menos dois amigos para sortear.");
+        erroListaAmigosVazia();
         return; // Se houver menos de dois amigos, não faz nada 
     } else {
         if(amigos.length === amigosSorteados.length){
             mostrarResultado("Todos os amigos já foram sorteados.");
             return; // Se todos os amigos já foram sorteados, não faz nada
         }
-           let tamanhoArray = amigos.length;
-           let numeroAleatorio = Math.floor(Math.random() * tamanhoArray);
-           let amigoSecreto = amigos[numeroAleatorio];
-              if(verificarAmigosSorteados(amigoSecreto)){
-                sortearAmigo(); // Se o amigo já foi sorteado, chama a função novamente
-            }else{
-                amigosSorteados.push(amigoSecreto); // Adiciona o amigo sorteado ao array
-                mostrarResultado(`O amigo sorteado é: <strong>${amigoSecreto}</strong>`);
-                
-            }
+        let amigoSecreto = sortear(); // Chama a função para sortear um amigo
+        if(verificarAmigosSorteados(amigoSecreto)){
+            sortearAmigo(); // Se o amigo já foi sorteado, chama a função novamente
+        }else{
+            amigosSorteados.push(amigoSecreto); // Adiciona o amigo sorteado ao array;
+            sucessoResultado(amigoSecreto); // Exibe a mensagem de sucesso
+            destacarAmigoSorteado(amigoSecreto); // Destaca o amigo sorteado na lista
+
            
+            
+        }
     }
+}
+
+function sucessoResultado(amigoSecreto){
+    document.getElementById("resultado").classList.remove("result-list-vazia");
+    document.getElementById("resultado").classList.add("result-list");
+    mostrarResultado(`O amigo sorteado é: <strong>${amigoSecreto}</strong>`, "result-list");
 }
 
 function mostrarResultado(mensagem){
     document.getElementById("resultado").innerHTML = mensagem;
 }
 
-function mostrarListaAmigos(){
+function mostrarListaAmigos(classList){
+
     let lista = document.getElementById("listaAmigos");
     lista.innerHTML = ""; // Limpa a lista antes de exibir os amigos
     for(let i = 0; i< amigos.length; i++){
@@ -70,5 +79,26 @@ function verificarAmigosSorteados(amigo){
     return false; // O amigo não foi sorteado
 }
 
+function sortear(){
+    let tamanhoArray = amigos.length;
+    let numeroAleatorio = Math.floor(Math.random() * tamanhoArray);
+    return amigos[numeroAleatorio];
+}
 
+function destacarAmigoSorteado(amigo){
+    let lista = document.getElementById("listaAmigos");
+    let itens = lista.getElementsByTagName("li");
+    for(let i = 0; i < itens.length; i++){
+        if(itens[i].textContent === amigo){
+            itens[i].classList.add("amigo-sorteado"); // Adiciona uma classe para destacar o amigo sorteado
+        }
+    }
+
+}
+
+function erroListaAmigosVazia(){
+    document.getElementById("resultado").classList.remove("result-list");
+    document.getElementById("resultado").classList.add("result-list-vazia");
+    mostrarResultado("Adicione pelo menos dois amigos para sortear.");
+}
 
